719e369da0b13a0efbac0e15fb18432c589d5fa5
Лилия <l_loshakova123@mail.ru>
2023-01-09 20:57:33 +0300

    New file

---
+++ StudentComparatorById.java
@@ -0,0 +1,12 @@
+package School;
+
+import java.util.Comparator;
+
+public class StudentComparatorById implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return 0;
+    }
+   
+    
+}
