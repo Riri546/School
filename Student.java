79a68af89bde46a4ac3906e987e4dbd8d348f29e
Лилия <l_loshakova123@mail.ru>
2023-01-09 20:21:21 +0300

    Sketch

--- Student.java
+++ Student.java
@@ -24,4 +24,9 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return id + " " + name;
+    }
 }
