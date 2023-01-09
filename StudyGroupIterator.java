fe9a275a23622375e7113d932500e2a27152dbad
Лилия <l_loshakova123@mail.ru>
2023-01-09 20:36:12 +0300

    New file

--- StudyGroupIterator.java
+++ StudyGroupIterator.java
@@ -1,8 +1,16 @@
 package School;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class StudyGroupIterator implements Iterator<Student> {
 
     private int index;
+    private List<Student> studentList;
+
+    public StudyGroupIterator(List<Student> studentList){
+        this.studentList = studentList;
+    }
 
     @Override
     public boolean hasNext() {
