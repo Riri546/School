5c89d7c9acf52c6b0dd1643b4ca30eb6acedd3d3
Лилия <l_loshakova123@mail.ru>
2023-01-09 19:45:09 +0300

    New file

---
+++ StudyGroup.java
@@ -0,0 +1,12 @@
+package School;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudyGroup {
+    private List<Student> studentList;
+
+    public StudyGroup(){
+        studentList = new ArrayList<>();
+    }
+}
